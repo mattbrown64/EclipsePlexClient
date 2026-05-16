1094ebff983bcfa044bd2d08365401420c152d45
mattbrown64 <mattbrown4404@gmail.com>
2026-05-15 21:35:30 -0400

    Initial Commit

---
+++ EclipsePlexClient/EclipsePlexClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  EclipsePlexClientApp.swift
+//  EclipsePlexClient
+//
+//  Created by Matt Brown on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct EclipsePlexClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EclipsePlexClientTests/EclipsePlexClientTests.swift
@@ -0,0 +1,18 @@
+//
+//  EclipsePlexClientTests.swift
+//  EclipsePlexClientTests
+//
+//  Created by Matt Brown on 5/15/26.
+//
+
+import Testing
+
+struct EclipsePlexClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
